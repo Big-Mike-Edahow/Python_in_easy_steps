ee1e8389e0f438f755a259bd67629132f8042746
Mike Jackson <perigran_falcon@yahoo.com>
2023-03-16 11:36:59 -0600

    Add files via upload

---
+++ list.py
@@ -0,0 +1,14 @@
+# list.py
+
+quarter = ["January", "February", "March"]
+
+print("First Month: ", quarter[0])
+print("Second Month: ", quarter[1])
+print("Third Month: ", quarter[2], "\n")
+
+coords = [[1, 2, 3], [4, 5, 6]]
+
+print("Top Left 0,0: ", coords[0][0])
+print("Bottom Right 1,2: ", coords[1][2], "\n")
+
+print("Second Month First Letter: ", quarter[1][0])
