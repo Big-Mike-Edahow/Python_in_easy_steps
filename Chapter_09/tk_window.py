f929c03029ea6032a3aeb26c853ea30f7cb63586
Mike Jackson <perigran_falcon@yahoo.com>
2023-12-12 07:55:22 -0700

    Add files via upload

---
+++ Chapter_09/tk_window.py
@@ -0,0 +1,12 @@
+# tk_window.py
+
+from tkinter import *
+
+window = Tk()
+window.title('Label Example')
+
+label = Label(window, text = 'Hello World!')
+label.pack(padx = 200, pady = 50)
+
+window.mainloop()
+
